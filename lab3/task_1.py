9f3fee085652718091997bf169bd86ab34c30e8d
lomariavl <lobanovamaria00@mail.ru>
2024-01-23 20:25:15 +0300

    Лабораторная работа 3

---
+++ lab3/task_1.py
@@ -0,0 +1,13 @@
+# Функция для поиска индекса товара
+def id_item(items: list, item: str):
+    return items.index(item) if item in items else None
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = id_item(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
